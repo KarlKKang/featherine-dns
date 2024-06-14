ef63dc3b53877fbbd19199c053c741d63a2446a5
KarlKKang <karl_kai.kang@icloud.com>
2024-06-14 04:24:20 -0400

    Revert to parallel DNS lookup. Doing in sequence doesn't help with the hopping issue.
    This reverts commit b0b65f22c368c9e257a87ee97f12e0859d18308f.

--- update/index.js
+++ update/index.js
@@ -63,7 +63,7 @@ async function validateLocation(hostname, ip, code) {
  * @param {"A"|"AAAA"} type
  * @param {string[]} ipList
  */
-function toChangeObj(hostname, type, ipList) {
+async function toChangeObj(hostname, type, ipList) {
     return /** @type {const} */ ({
         Action: 'UPSERT',
         ResourceRecordSet: {
@@ -73,6 +73,20 @@ function toChangeObj(hostname, type, ipList) {
             ResourceRecords: ipList.map(ip => ({ Value: ip }))
         }
     });
+}
+
+/**
+ * @param {string} domain
+ * @param {string} code
+ * @param {"A"|"AAAA"} type
+ * @param {string} subnet
+ */
+async function getChangeObj(domain, code, type, subnet) {
+    const ipList = await dnsLookup(domain, type, subnet);
+    if (type === 'A') {
+        await validateLocation(domain, ipList[0], code);
+    }
+    return toChangeObj(code + '.' + domain, type, ipList);
 }
 
 /**
@@ -91,48 +105,28 @@ async function main() {
     /** @type {{id: string, location: string, country: string, subnet: string, code: string}[]} */
     const pops = JSON.parse(readFileSync(path.join(__dirname, '..', 'pop.json'), 'utf8'));
 
-    /** @type {{domain: string, code: string, ipList: Awaited<ReturnType<typeof dnsLookup>>}[]} */
-    const ipv4List = [];
-    /** @type {typeof ipv4List} */
-    const ipv6List = [];
+    /** @type {ReturnType<typeof getChangeObj>[]} */
+    const promises = [];
     for (const domain of DOMAINS) {
         for (const pop of pops) {
-            pop.code = pop.code.toLowerCase();
-            ipv4List.push({
-                domain: domain,
-                code: pop.code,
-                ipList: await dnsLookup(domain, 'A', pop.subnet)
-            });
-            ipv6List.push({
-                domain: domain,
-                code: pop.code,
-                ipList: await dnsLookup(domain, 'AAAA', pop.subnet)
-            });
+            promises.push(getChangeObj(domain, pop.code.toLowerCase(), 'A', pop.subnet));
+            promises.push(getChangeObj(domain, pop.code.toLowerCase(), 'AAAA', pop.subnet));
         }
-    }
-
-    const ipv4ChangeObjPromises = ipv4List.map(async ({ domain, code, ipList }) => {
-        await validateLocation(domain, ipList[0], code);
-        return toChangeObj(code + '.' + domain, 'A', ipList);
-    });
-    const results = ipv6List.map(({ domain, code, ipList }) => toChangeObj(code + '.' + domain, 'AAAA', ipList));
-
-    const ipv4ChangeObjs = await Promise.allSettled(ipv4ChangeObjPromises);
-    for (const ipv4ChangeObj of ipv4ChangeObjs) {
-        if (ipv4ChangeObj.status === 'rejected') {
-            console.error(ipv4ChangeObj.reason);
-            continue;
-        }
-        results.push(ipv4ChangeObj.value);
     }
 
     /** @type {{Changes: Awaited<ReturnType<typeof toChangeObj>>[]}} */
     let currentChangeBatch = { Changes: [] };
     let changeBatches = [currentChangeBatch];
     let characterCount = 0;
-    for (const result of results) {
+    let promiseResults = await Promise.allSettled(promises);
+    for (const result of promiseResults) {
+        if (result.status === 'rejected') {
+            console.error(result.reason);
+            continue;
+        }
+        const changeObj = result.value;
         let currentCharacterCount = 0;
-        for (const ip of result.ResourceRecordSet.ResourceRecords) {
+        for (const ip of changeObj.ResourceRecordSet.ResourceRecords) {
             currentCharacterCount += ip.Value.length;
         }
         characterCount += currentCharacterCount;
@@ -141,7 +135,7 @@ async function main() {
             changeBatches.push(currentChangeBatch);
             characterCount = currentCharacterCount;
         }
-        currentChangeBatch.Changes.push(result);
+        currentChangeBatch.Changes.push(result.value);
     }
 
     for (const changeBatch of changeBatches) {
