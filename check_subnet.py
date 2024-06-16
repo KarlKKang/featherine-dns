5bb35f06d9fcb27d63416bea32a7121aa3c25bba
KarlKKang <karl_kai.kang@icloud.com>
2024-06-16 08:37:43 -0400

    Don't throw in the check_subnet script Read the file in absolute path in the check_subnet script

--- check_subnet.py
+++ check_subnet.py
@@ -2,22 +2,26 @@ import json
 import sys
 import subprocess
 import ipaddress
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def inverse_lookup(ip):
-    p = subprocess.run(
-        ["dig", "-x", ip, "+short"], check=True, capture_output=True, text=True
-    )
+    p = subprocess.run(["dig", "-x", ip, "+short"], capture_output=True, text=True)
+    if p.returncode != 0:
+        return ""
     return p.stdout.strip()
 
 
 def dns_lookup(domain, subnet):
     p = subprocess.run(
         ["dig", "@8.8.8.8", domain, "A", "+short", "+subnet=" + subnet],
-        check=True,
         capture_output=True,
         text=True,
     )
+    if p.returncode != 0:
+        return None
     for line in p.stdout.splitlines():
         try:
             ipaddress.ip_address(line)
@@ -33,7 +37,7 @@ def main():
         print("Usage: python check_subnet.py <domain>")
         sys.exit(1)
 
-    with open("pop.json") as f:
+    with open(os.path.join(SCRIPT_DIR, "pop.json")) as f:
         pops = json.load(f)
 
     for pop in pops:
@@ -41,13 +45,13 @@ def main():
         code = pop["code"]
         ip = dns_lookup(domain, subnet)
         if ip is None:
-            raise ValueError(f"Cannot find IP address for {code}")
+            print(f"Cannot find IP address for {code}")
         dns_name = inverse_lookup(ip)
         dns_name_prefix = "server-" + ip.replace(".", "-") + "." + code.lower()
         if dns_name.startswith(dns_name_prefix):
             print(f"{code} is OK")
         else:
-            raise ValueError(f"{code} gets unexpected DNS name {dns_name}")
+            print(f"{code} gets unexpected DNS name {dns_name}")
 
 
 if __name__ == "__main__":
